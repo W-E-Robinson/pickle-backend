27eb7b02c1d4a50b4a2d57388fa18df9bc753f39
Will Robinson <william.ellis.robinson@gmail.com>
2023-01-22 21:50:20 +0000

    script adding 4/5

--- scripts.sql
+++ scripts.sql
@@ -1,8 +1,16 @@
 -- GET /lists/{userId}
+-- TO DO
+SELECT
+    l.listId,
+    l.list
+FROM lists l
+WHERE l.listId = {listId};
 
--- PUT /lists body={ itemId: number, ingredient: string, quantity: number, completed: boolean }
+-- PUT /lists body={ itemId: number, ingredient: string, quantity: number, completed: boolean }[]
+-- TO DO
 
 -- GET /dishes
+-- DONE
 SELECT
     d.dishId,
     d.title,
@@ -16,15 +24,46 @@ SELECT
 FROM dishes d
 INNER JOIN users u ON d.userId = u.userId
 ORDER BY 1 DESC;
+
 -- GET /dishes?cuisine=___&diet=___&time=___
+-- TO DO
 
 -- GET /savedDishes/{userId}
+-- DONE
+select
+	sub.dishId,
+	d.title,
+    d.picture,
+    d.cuisines,
+    d.dietRestrictions,
+    d.time,
+    u.name,
+    u.location,
+    u.picture
+FROM
+(
+	SELECT
+	   	unnest(u.savedDishes) AS dishId
+	FROM users u
+	WHERE u.userid = {userId}
+) AS sub
+INNER JOIN dishes d ON sub.dishId = d.dishid
+INNER JOIN users u ON d.userId = u.userId
+ORDER BY 1 desc;
 
--- PUT /savedDishes body={ dishId: number }
+-- PATCH /savedDishes body={ dishId: number, action: "removal" | "addition" }
+-- TO DO
 
--- GET /searchFilters?filter=cuisine
+-- GET /searchFilters?filter=cuisines
+-- DONE
+SELECT
+    DISTINCT UNNEST(d.cuisines)
+FROM dishes d
+ORDER BY 1 ASC;
 
--- GET /searchFilters?filter=diet
-
--- GET /searchFilters?filter=time
-
+-- GET /searchFilters?filter=diets
+-- DONE
+SELECT
+    DISTINCT UNNEST(d.dietRestrictions)
+FROM dishes d
+ORDER BY 1 ASC;
